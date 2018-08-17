2bedb002a7266c337c5e445acb460ab80915eda2
Stephen Celis <stephen@stephencelis.com>
2018-08-17 00:47:28 -0400

    WIP

--- Tests/OvertureTests/ZipTests.swift
+++ Tests/OvertureTests/ZipTests.swift
@@ -31,19 +31,40 @@ final class ZipTests: XCTestCase {
   }
 
   func testZipSequence() {
-    let (a, b, c, d, e, f, g, h, i, j) = Array(
-      zip(
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-        [10, 11, 12],
-        [13, 14, 15],
-        [16, 17, 18],
-        [19, 20, 21],
-        [22, 23, 24],
-        [25, 26, 27],
-        [28, 29, 30]
-      )
+    let zipped = zip(
+      [1, 2],
+      [3, 4],
+      [5, 6],
+      [7, 8],
+      [9, 10],
+      [11, 12],
+      [13, 14],
+      [15, 16],
+      [17, 18],
+      [19, 20]
     )
+    let array = Array(zipped)
+    let fst = array[0]
+    XCTAssertEqual(1, fst.0)
+    XCTAssertEqual(3, fst.1)
+    XCTAssertEqual(5, fst.2)
+    XCTAssertEqual(7, fst.3)
+    XCTAssertEqual(9, fst.4)
+    XCTAssertEqual(11, fst.5)
+    XCTAssertEqual(13, fst.6)
+    XCTAssertEqual(15, fst.7)
+    XCTAssertEqual(17, fst.8)
+    XCTAssertEqual(19, fst.9)
+    let snd = array[1]
+    XCTAssertEqual(2, snd.0)
+    XCTAssertEqual(4, snd.1)
+    XCTAssertEqual(6, snd.2)
+    XCTAssertEqual(8, snd.3)
+    XCTAssertEqual(10, snd.4)
+    XCTAssertEqual(12, snd.5)
+    XCTAssertEqual(14, snd.6)
+    XCTAssertEqual(16, snd.7)
+    XCTAssertEqual(18, snd.8)
+    XCTAssertEqual(20, snd.9)
   }
 }
