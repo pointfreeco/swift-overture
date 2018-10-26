1a6e9278e4ab5f58ce12d0ce1a6101f2be1b890d
Olli Tapaninen <olli.tapaninen@bitfactor.fi>
2018-10-26 10:38:52 +0300

    iOS target

---
+++ Overture-iOS/Overture_iOS.h
@@ -0,0 +1,18 @@
+//
+//  Overture_iOS.h
+//  Overture-iOS
+//
+//  Created by Olli Tapaninen on 26/10/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Overture_iOS.
+FOUNDATION_EXPORT double Overture_iOSVersionNumber;
+
+//! Project version string for Overture_iOS.
+FOUNDATION_EXPORT const unsigned char Overture_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Overture_iOS/PublicHeader.h>
+
+
